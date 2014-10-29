1d25c6c53e766d9a2d6177e052ba0adf7e9772be
duncan heron <duncan@Duncans-Air.local>
2014-10-29 17:15:06 +0000

    tabs to spaces

--- app/database/seeds/PlayerSeeder.php
+++ app/database/seeds/PlayerSeeder.php
@@ -2,40 +2,40 @@
 
 class PlayerSeeder extends Seeder {
 
-	/**
-	 * Run the database seeds.
-	 *
-	 * @return void
-	 */
-	public function run()
-	{
-		Eloquent::unguard();
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Eloquent::unguard();
 
-		$this->command->info('Deleting existing Players ...');
+        $this->command->info('Deleting existing Players ...');
         DB::table('players')->delete();
 
-		$player = User::create(
-			array(
-	        	'firstname' => 'Duncan',
-	        	'lastname' => 'Heron',
-	            'email' => 'duncanuk@gmail.com',
-	            'password' => Hash::make('test')
-	        )
-	    );
-	    $faker = Faker\Factory::create();
+        $player = User::create(
+            array(
+                'firstname' => 'Duncan',
+                'lastname' => 'Heron',
+                'email' => 'duncanuk@gmail.com',
+                'password' => Hash::make('test')
+            )
+        );
+        $faker = Faker\Factory::create();
  
-		for ($i = 0; $i < 10; $i++)	{
-			$player = User::create(
-				array(
-					'firstname' => $faker->firstName,
-					'lastname' => $faker->lastName,
-					'email' => $faker->email,
-					'password' => Hash::make('test')
-				)
-			);
-		}
-		
-		$this->command->info('Players table seeded using Faker ...');
-	}
+        for ($i = 0; $i < 10; $i++) {
+            $player = User::create(
+                array(
+                    'firstname' => $faker->firstName,
+                    'lastname' => $faker->lastName,
+                    'email' => $faker->email,
+                    'password' => Hash::make('test')
+                )
+            );
+        }
+        
+        $this->command->info('Players table seeded using Faker ...');
+    }
 
 }
